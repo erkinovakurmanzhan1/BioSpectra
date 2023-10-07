30d9164265f5641e7458bec4d82aa4ef5d89d196
Kurmanzhan <erkinovakurmanjan960@gmail.com>
2023-10-07 22:42:10 +0300

    start project

---
+++ src/routes/AppRoute.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppRoute = () => {
+  return <div>AppRoute</div>;
+};
+
+export default AppRoute;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Bai Jamjuree, Graphik", "Inter", "sans-serif,"],
+        serif: ["Merriweather", "serif"],
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "url('https://i.pinimg.com/564x/67/6a/77/676a77e2614f48ff5a3befca984d5bd2.jpg')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: false,
+    utils: false,
+    logs: false,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
+};
