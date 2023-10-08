b73a172a3d53dfb3bc71a69e952b6e8ff2133abf
Kurmanzhan <erkinovakurmanjan960@gmail.com>
2023-10-08 04:29:49 +0300

    header,footer,cards done

---
+++ src/components/card/WorkShopCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { workShopData } from "../../constants/workshopData";
+
+const WorkShopCard = () => {
+  return (
+    <div className="bg-[#F0FFFB] p-[6.25rem]">
+      <h1 className="text-5xl font-semibold pb-[3rem]">Participate in Our Workshops</h1>
+      <div className="flex gap-6 ">
+        {workShopData.map((item) => (
+          <div className="bg-white  w-[18.25rem] rounded-xl">
+            <div>
+              <img src={item.images} alt="" className="rounded-xl"/>
+            </div>
+            <div className="px-6 py-6">
+              <h1 className="text-3xl font-semibold">{item.title}</h1>
+              <div className="flex gap-2 items-center text-base py-4">
+                <img src={item.calendar} alt="calendar" />
+                <p>{item.date}</p>
+              </div>
+              <div className="flex gap-2 items-center text-base pb-9">
+                <img src={item.map} alt="map" />
+                <p>{item.geo}</p>
+              </div>
+              <button className="py-2 px-16 bg-[#1DB688] text-white rounded-lg">
+                Readmore
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WorkShopCard;
---
+++ src/constants/workshopData.js
@@ -0,0 +1,42 @@
+import workshopImages from '../assets/images/Frame 187.png'
+import calendarIcon from '../assets/icons/mdi_calendar-range.svg'
+import mapIcon from '../assets/icons/mdi_map-marker.svg'
+
+
+export const workShopData = [
+    {
+        id: 1,
+        images: workshopImages,
+        title: 'Workshop 1',
+        calendar: calendarIcon,
+        date: '18-21 June',
+        map: mapIcon,
+        geo: '18-21 June'
+    },
+    {
+        id: 2,
+        images: workshopImages,
+        title: 'Workshop 1',
+        calendar: calendarIcon,
+        date: '18-21 June',
+        map: mapIcon,
+        geo: '18-21 June'
+    }, {
+        id: 3,
+        images: workshopImages,
+        title: 'Workshop 1',
+        calendar: calendarIcon,
+        date: '18-21 June',
+        map: mapIcon,
+        geo: '18-21 June'
+    }, {
+        id: 4,
+        images: workshopImages,
+        title: 'Workshop 1',
+        calendar: calendarIcon,
+        date: '18-21 June',
+        map: mapIcon,
+        geo: '18-21 June'
+    },
+
+]
